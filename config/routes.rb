78b0039933d825338c08a8ded57b014604ad668f
Nick Weiland <nick.weiland@stacksocial.com>
2016-06-17 17:27:03 -0700

    get dat style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      jsonapi_resources :styles, only: [:index, :create]
+      jsonapi_resources :styles, only: [:index, :create, :show]
     end
   end
 end
--- spec/requests/api/v1/styles_spec.rb
+++ spec/requests/api/v1/styles_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe "api/v1/styles", type: :request do
       expect(Style.first.style_attributes).to eq({ hey: 'world' })
     end
   end
+
+  describe '#show' do
+    it 'displays the style' do
+      publisher = create(:publisher)
+      publisher.styles.create(style_attributes: { hello: 'world'})
+      style_two = publisher.styles.create(style_attributes: { goodbye: 'world'})
+
+      get api_v1_styles_path(style_two)
+
+      response_body = JSON.parse(response.body)
+      attributes = response_body['data'].last['attributes']
+
+      expect(response).to have_http_status(200)
+      expect(attributes['style-attributes']).to eq({ 'goodbye' => 'world' })
+    end
+  end
 end
